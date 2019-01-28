7729dc7a69b06f95eb642c672f10c91902267d6a
Ransford Walker <ransfordw@gmail.com>
2019-01-28 09:47:13 -0500

    added IDiceRoller interface

---
+++ DiceRoller/IDiceRoller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public interface IDiceRoller
+    {
+        bool ReturnBoolean(string s);
+        List<int> GetResults(int number, int choice);
+        int GetRandomNumber(int i);
+    }
+}
