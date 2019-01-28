94b2262d3b6874ed0941281a2298c20239515cc5
Ransford Walker <ransfordw@gmail.com>
2019-01-28 14:13:38 -0500

    Unit Testing: Added MockConsole, Renamed repository tests

--- DiceRoller.Tests/DiceRollerRepositoryTests.cs
+++ DiceRoller.Tests/DiceRollerRepositoryTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DiceRoller.Tests
 {
     [TestClass]
-    public class DiceRollerTest
+    public class DiceRollerRepositoryTests
     {
         DiceRollerRepository _diceRepo;
         [TestInitialize]
